de068b9be822478871e83d7ec15ad6774e6853fd
morakfilmali <sharif.afroosheh@gmail.com>
2022-02-17 21:10:39 +0330

    w
    h

---
+++ php/admin_area/index.php
@@ -0,0 +1,25 @@
+<!-- start of Header -->
+<?php	include('include/Header.php');	?>
+<!-- end of Header -->				
+
+<div id="templatemo_content">
+	
+	
+	<!-- start of ocntent left -->
+	<div id="templatemo_content_left">
+		
+		
+		<div class="cleaner_with_height">&nbsp;</div>
+	</div> 
+	<!-- end of ocntent left -->
+	
+	<!-- start of right content -->
+	<?php	include('include/Right_Sidebar.php');	?>
+	<!-- end of right content -->
+	
+	<div class="cleaner">&nbsp;</div>
+</div>
+
+<!-- start of footer -->
+<?php include('include/Footer.php');	?>
+<!-- end of footer -->

fd265a1761c03db94fc6f60a4196da6d4f7bc301
morakfilmali <sharif.afroosheh@gmail.com>
2022-02-22 09:33:42 +0330

    1
    بل

--- wp-content/plugins/elespare/header-footer/templates/content/content-footer.php
+++
@@ -1,5 +0,0 @@
-<footer id="mastfooter" class="elespare-site-footer">
-	<div class="elespare-footer-inner">
-		<?php the_content(); ?>
-	</div>
-</footer>
--- wp-content/plugins/elespare/header-footer/templates/content/content-header.php
+++
@@ -1,5 +0,0 @@
-<header id="masthead" class="elespare-site-header">
-	<?php the_content(); ?>
-</header>
-
-<?php 
